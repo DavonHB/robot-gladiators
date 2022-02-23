47594028dc5b603800f82fac4323b23ae412b4fb
Nosferatu <davonb113@gmail.com>
2022-02-23 11:59:45 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,14 +21,16 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -47,7 +49,9 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+     
+      playerHealth = Math.max(0, playerHealth - damage);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -74,7 +78,7 @@ var startGame = function() {
 
         var pickedEnemyName = enemyNames[i];
 
-        enemyHealth = 50;
+        enemyHealth = randomNumber(40, 60);
 
         fight(pickedEnemyName);
 
@@ -89,6 +93,7 @@ var startGame = function() {
 
       else {
           window.alert("You have lost your robot in battle! Gamde Over!");
+          break;
       }
   }
   endGame();
@@ -148,12 +153,23 @@ var shop = function() {
 
         case "LEAVE":
         case "leave": 
-            window.alert("You did not pick a valid option. Try again.");
+            window.alert("Leaving the store.");
+            break;
+        
+        default:
+            window.alert("You did not pick a valid option. Try again.")
 
             shop();
             break;
-    }
-}
+    };
+
+};
+
+var randomNumber = function () {
+  var value = Math.floor(Math.random() * 21) + 40;
+
+  return value;
+};
 
 
 startGame();
